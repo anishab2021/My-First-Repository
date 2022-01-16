b05cba01175f00b9e7f4f62e45615c1e5de69dd9
anishab2021 <93690730+anishab2021@users.noreply.github.com>
2022-01-16 17:53:27 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(400,400);
+  var ball = createSprite(200,200,50,50);
+  ball.shapeColor = "blue";
+}
+
+function draw() 
+{
+  background(180);
+drawSprites();
+}
+
+
+
+
